2475d0d5adaf56fa108843b4a9891c215221a9c2
Royce Chua <royce236@gmail.com>
2021-01-18 22:49:54 +0800

    Add config for redux store with persist and thunk

---
+++ src/redux/store/index.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-community/async-storage';
+
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/index';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default () => {
+  let store = createStore(persistedReducer, applyMiddleware(thunk))
+  let persistor = persistStore(store)
+  return { store, persistor }
+}
+
