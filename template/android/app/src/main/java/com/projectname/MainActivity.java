98b99052117e79093e7041808b1136f8956bbfbb
Royce Chua <royce236@gmail.com>
2021-02-08 02:23:44 +0800

    Fix project name

--- template/android/app/src/main/java/com/projectname/MainActivity.java
+++ template/android/app/src/main/java/com/projectname/MainActivity.java
@@ -1,4 +1,4 @@
-package com.projectName;
+package com.projectname;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "projectName";
+    return "projectname";
   }
 }
