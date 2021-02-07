78047d77f89159de7fab2c682f5d3d506cd36b56
Royce Chua <royce236@gmail.com>
2021-02-07 13:58:05 +0800

    Add Redux Store and Persistor

--- App.js
+++ App.js
@@ -9,17 +9,23 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   StatusBar,
 } from 'react-native';
+import { store, persistor } from './src/redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 const App = () => {
   return (
     <>
-      <StatusBar barStyle="default" />
-      <SafeAreaView>
-        
-      </SafeAreaView>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <StatusBar barStyle="default" />
+          <SafeAreaView>
+            
+          </SafeAreaView>
+        </PersistGate>
+      </Provider>
     </>
   );
 };
