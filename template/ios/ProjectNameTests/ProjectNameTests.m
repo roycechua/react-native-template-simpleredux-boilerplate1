a16357637b88967f23321c2ea8578643e3e526ae
Royce Chua <royce236@gmail.com>
2021-02-09 09:07:20 +0800

    Add new iOS project folder

--- template/ios/ProjectNameTests/ProjectNameTests.m
+++ template/ios/ProjectNameTests/ProjectNameTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface projectNameTests : XCTestCase
+@interface ProjectNameTests : XCTestCase
 
 @end
 
-@implementation projectNameTests
+@implementation ProjectNameTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
